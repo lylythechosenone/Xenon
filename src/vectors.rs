bb14dbfc71be36d4f252af5c53b34f32f98579cd
lylythechosenone <lmealy2022@ms447.org>
2022-10-16 17:19:23 -0400

    Add more vectors

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,59 +1,55 @@
 macro_rules! vector_impl {
-    ($ident:ident, $($component:ident),*) => {
-        impl std::ops::Add for $ident {
+    ($ident:ident) => {
+        impl<T: Vector2> std::ops::Add<T> for $ident {
             type Output = Self;
-            fn add(mut self, other: Self) -> Self {
+            fn add(mut self, other: T) -> Self {
                 self += other;
                 self
             }
         }
-        impl std::ops::AddAssign for $ident {
-            fn add_assign(&mut self, other: Self) {
-                *self = Self {
-                    $($component: self.$component + other.$component),*
-                };
+        impl<T: Vector2> std::ops::AddAssign<T> for $ident {
+            fn add_assign(&mut self, other: T) {
+                self.x += other.x();
+                self.y += other.y();
             }
         }
-        impl std::ops::Sub for $ident {
+        impl<T: Vector2> std::ops::Sub<T> for $ident {
             type Output = Self;
-            fn sub(mut self, other: Self) -> Self {
+            fn sub(mut self, other: T) -> Self {
                 self -= other;
                 self
             }
         }
-        impl std::ops::SubAssign for $ident {
-            fn sub_assign(&mut self, other: Self) {
-                *self = Self {
-                    $($component: self.$component - other.$component),*
-                };
+        impl<T: Vector2> std::ops::SubAssign<T> for $ident {
+            fn sub_assign(&mut self, other: T) {
+                self.x -= other.x();
+                self.y -= other.y();
             }
         }
-        impl std::ops::Mul for $ident {
+        impl<T: Vector2> std::ops::Mul<T> for $ident {
             type Output = Self;
-            fn mul(mut self, other: Self) -> Self {
+            fn mul(mut self, other: T) -> Self {
                 self *= other;
                 self
             }
         }
-        impl std::ops::MulAssign for $ident {
-            fn mul_assign(&mut self, other: Self) {
-                *self = Self {
-                    $($component: self.$component * other.$component),*
-                };
+        impl<T: Vector2> std::ops::MulAssign<T> for $ident {
+            fn mul_assign(&mut self, other: T) {
+                self.x *= other.x();
+                self.y *= other.y();
             }
         }
-        impl std::ops::Div for $ident {
+        impl<T: Vector2> std::ops::Div<T> for $ident {
             type Output = Self;
-            fn div(mut self, other: Self) -> Self {
+            fn div(mut self, other: T) -> Self {
                 self /= other;
                 self
             }
         }
-        impl std::ops::DivAssign for $ident {
-            fn div_assign(&mut self, other: Self) {
-                *self = Self {
-                    $($component: self.$component / other.$component),*
-                };
+        impl<T: Vector2> std::ops::DivAssign<T> for $ident {
+            fn div_assign(&mut self, other: T) {
+                self.x /= other.x();
+                self.y /= other.y();
             }
         }
         impl std::ops::Mul<f32> for $ident {
@@ -65,9 +61,8 @@ macro_rules! vector_impl {
         }
         impl std::ops::MulAssign<f32> for $ident {
             fn mul_assign(&mut self, other: f32) {
-                *self = Self {
-                    $($component: self.$component * other),*
-                };
+                self.x *= other;
+                self.y *= other;
             }
         }
         impl std::ops::Div<f32> for $ident {
@@ -79,9 +74,8 @@ macro_rules! vector_impl {
         }
         impl std::ops::DivAssign<f32> for $ident {
             fn div_assign(&mut self, other: f32) {
-                *self = Self {
-                    $($component: self.$component / other),*
-                };
+                self.x /= other;
+                self.y /= other;
             }
         }
         impl std::ops::Neg for $ident {
@@ -92,9 +86,9 @@ macro_rules! vector_impl {
             }
         }
         impl $ident {
-            pub const fn new($($component: f32),*) -> Self {
+            pub const fn new(x: f32, y: f32) -> Self {
                 Self {
-                    $($component),*
+                    x, y
                 }
             }
             pub fn length(&self) -> f32 {
@@ -103,23 +97,61 @@ macro_rules! vector_impl {
             pub fn length_squared(&self) -> f32 {
                 self.dot(self.clone())
             }
-            pub fn dot(&self, other: Self) -> f32 {
-                0.0 $(+ self.$component * other.$component)*
+            pub fn dot(&self, other: impl Vector2) -> f32 {
+                self.x * other.x() + self.y * other.y()
+            }
+            pub fn min(&self, other: Self) -> Self {
+                if *self < other {
+                    self.clone()
+                } else {
+                    other
+                }
+            }
+            pub fn max(&self, other: Self) -> Self {
+                if *self > other {
+                    self.clone()
+                } else {
+                    other
+                }
+            }
+            pub fn lerp(&self, other: Self, t: f32) -> Self {
+                self.clone() + (self.clone() - other) * t
+            }
+        }
+        impl Vector2 for $ident {
+            fn x(&self) -> f32 {
+                self.x
+            }
+            fn y(&self) -> f32 {
+                self.y
             }
         }
     };
 }
 
-#[derive(Clone, Debug, PartialEq, PartialOrd)]
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
 pub struct Point2 {
     pub x: f32,
     pub y: f32,
 }
-vector_impl!(Point2, x, y);
+vector_impl!(Point2);
 
-#[derive(Clone, Debug, PartialEq, PartialOrd)]
-pub struct Vector2 {
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
+pub struct Extent2 {
     pub x: f32,
     pub y: f32,
 }
-vector_impl!(Vector2, x, y);
+vector_impl!(Extent2);
+impl Extent2 {
+    pub fn as_point(self) -> Point2 {
+        Point2 {
+            x: self.x,
+            y: self.y,
+        }
+    }
+}
+
+pub trait Vector2 {
+    fn x(&self) -> f32;
+    fn y(&self) -> f32;
+}
